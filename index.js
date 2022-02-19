c8f7660912cd1f3a607575ea87f30eef58df2bb8
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-19 00:58:09 -0500

    completed first two challenges

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^\w\s\-\']/gi, "");
+  }
 }
